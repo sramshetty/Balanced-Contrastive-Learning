ccbebf0cf0bcd4594be5eb76ab54409bd2379b6c
tom30111 <69095843+tom30111@users.noreply.github.com>
2022-11-28 20:53:55 -0500

    Update focal.py

--- loss/focal.py
+++ loss/focal.py
@@ -24,13 +24,13 @@ class FocalLoss(nn.Module):
     
     def __init__(self, cls_num_list, weight=None, gamma=2., reduction=None):
         super(FocalLoss, self).__init__()
-        cls_num_list = torch.cuda.FloatTensor(cls_num_list)
-        cls_p_list = cls_num_list / cls_num_list.sum()
-        m_list = torch.log(cls_p_list)
-        self.m_list = m_list.view(1, -1)
-        self.weight = weight
-        self.gamma = gamma
-        self.reduction = reduction
+#         cls_num_list = torch.cuda.FloatTensor(cls_num_list)
+#         cls_p_list = cls_num_list / cls_num_list.sum()
+#         m_list = torch.log(cls_p_list)
+#         self.m_list = m_list.view(1, -1)
+#         self.weight = weight
+#         self.gamma = gamma
+#         self.reduction = reduction
         
 #     def forward(self, x, target):
 #         # log_prob = F.log_softmax(x, dim=-1)
